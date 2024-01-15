f70ac5a47b134dd272817953c994ec91cb16a5b7
jeanne_salle <jeanne.salle@yahoo.fr>
2024-01-15 20:52:47 +0100

    Updated parch_functions.py and base_options.py

--- base_options.py
+++ base_options.py
@@ -7,10 +7,19 @@ class BaseOptions():
         self.parser = argparse.ArgumentParser(formatter_class=argparse.ArgumentDefaultsHelpFormatter)
 
     def parse(self):
-        self.parser.add_argument('--source_model', '-M', type=str, default='gpt2_small',
-                                choices=['gpt2_small'], help='Source model architecture.')
-        self.parser.add_argument('--target_model', '-M*', type=str, default='gpt2_small',
-                                choices=['gpt2_small'], help='Target model architecture.')
+        self.parser.add_argument('--source_model', 
+                                '-M', 
+                                type=str, 
+                                default='gpt2_small',
+                                choices=['gpt2_small', 'pythia-6.9b'], 
+                                help='Source model architecture.')
+
+        self.parser.add_argument('--target_model', 
+                                '-M*', 
+                                type=str, 
+                                default='gpt2_small',
+                                choices=['gpt2_small', 'pythia-6.9b'], 
+                                help='Target model architecture.')
  
         
 
--- patch_functions.py
+++ patch_functions.py
@@ -1,5 +1,7 @@
 from transformers import GPTNeoXForCausalLM, AutoTokenizer, LlamaForCausalLM, LlamaTokenizer
 from transformer_lens import utils, HookedTransformer, ActivationCache, patching
+from transformer_lens.hook_points import HookPoint
+from transformer_lens.utils import get_act_name
 import torch
 from torch import Tensor
 from jaxtyping import Int, Float
@@ -14,59 +16,78 @@ def patchscope(opt, device):
     target_model = get_model(opt.target_model)
 
     source_prompt = "Amazon's former CEO attended Oscars"
-    target_prompt = "cat->cat; 135->135; hello->hello: ?"
-    #source_tokens = model.to_tokens(source_prompt, prepend_bos=True)
-    #source_tokens = source_tokens.to(device)
+    target_prompt = "cat->cat; 135->135; brain->brain; ?"
+    print('Source prompt:', source_prompt)
+    print('Source token:', source_model.to_str_tokens(source_prompt)[4])
+    source_position = 4
+
+    print('Target prompt:', target_prompt)
+    print('Target token:', target_model.to_str_tokens(target_prompt)[13])
+    target_position = 13
+
+    source_layer = target_layer = 4
+    print(f'Source layer: {source_layer}, Target layer: {target_layer}')
 
     _, source_cache = source_model.run_with_cache(source_prompt)
-    print(source_cache)
 
-    position = 0
-    layer = 2
     
-    patch_residual_stream(target_model, position, layer, target_prompt, source_cache)
+    patch_activations(
+        target_model, 
+        source_position, 
+        source_layer, 
+        target_position,
+        target_layer,
+        target_prompt, 
+        source_cache
+    )
 
     return 
 
 
 
-def get_model(model_name: str):
+def get_model(model_name: str = 'gpt2-small') -> HookedTransformer:
     """
     Loads source or target model.
+
+    model_name: ['gpt2-small', 'pythia-6.9b']
     """
-
-    if model_name == 'gpt2_small':
-        model = HookedTransformer.from_pretrained("gpt2-small")
-
-    return model
+    return HookedTransformer.from_pretrained(model_name)
 
 
-def patch_residual_stream(
+def patch_activations(
     target_model: HookedTransformer,
-    position: int,
-    layer: int,
+    source_position: int,
+    source_layer: int,
+    target_position: int,
+    target_layer: int,
     target_prompt: str,
-    source_cache: Float[Tensor, 'd_model']
+    source_cache: ActivationCache,
+    activation_type: str = 'resid_pre'
 ):
     """
-    Patches a residual stream vector into the target model.
+    Patches an activation vector into the target model.
     """
 
-    def hook_fn(activations: Float[Tensor, "batch seq_len"],
+    source_cache = source_cache[activation_type, source_layer]
+
+    def hook_fn(target_activations: Float[Tensor, '...'],
                 hook: HookPoint
-    ) -> TT["batch", "seq_len"]:
+    ) -> Float[Tensor, '...']:
+        target_activations[:,target_position,:] = source_cache[:,source_position,:]
+        return target_activations
 
-        # modify activations (can be inplace)
-        return activations
-
-    target_model.run_with_hooks(
-        target_tokens,
-        return_type=None,
+    target_logits = target_model.run_with_hooks(
+        target_prompt,
+        return_type="logits",
         fwd_hooks=[
-            (lambda name: name.endswith("resid_pre"), hook_fn)
+            (get_act_name(activation_type, target_layer), hook_fn)
         ]
     )
 
+    prediction = target_logits.argmax(dim=-1).squeeze()[:-1]
+    print(target_model.to_string(prediction[-1]))
+
+    return
 
 
 
