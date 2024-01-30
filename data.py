d4a642a41e85ef5343ef9115a531a4c02f7d279f
jeanne_salle <jeanne.salle@yahoo.fr>
2024-01-30 11:36:57 +0100

    Added data directory and get_S_from_wikitext

--- data.py
+++ data.py
@@ -13,14 +13,18 @@ def get_wikitext_103(split='train', n_samples=1):
     return data
 
 
+def get_task_type(task='country_currency'):
+    if task in ['country_currency', 'company_ceo', 'city_in_country']:
+        return 'factual'
+    elif task in ['fruit_inside_color', 'fruit outside_color', 'object_superclass']:
+        return 'commonsense' 
+
+
 def get_subobj_from_json(task='country_currency'):
     """
     Extract a list of subjects from json datasets provided by (Hernandez et al. 2023)
     """
-    if task in ['country_currency']:
-        dataset_path = os.path.join('relations/data/', 'factual/')
-    elif task in ['fruit_inside_color']:
-        dataset_path = os.path.join('relations/data/', 'commonsense/')
+    dataset_path = os.path.join('relations/data/', get_task_type(task)+'/')
 
     with open(dataset_path + task + '.json', 'r') as f:
         relations_dict = json.load(f)
@@ -37,33 +41,43 @@ def get_subobj_from_json(task='country_currency'):
 
 def get_S_from_wikitext(subjects_list, task):
 
-    source_prompts = pd.DataFrame(columns=['subject', 'S'])
-
-    dataset = load_dataset("iohadrubin/wikitext-103-raw-v1", split='train', streaming=True)
-
     headers = {"Authorization": f"Bearer {config.api_key}"}
-    API_URL = "https://datasets-server.huggingface.co/rows?dataset=iohadrubin%2Fwikitext-103-raw-v1&config=default&split=train&offset=0&length=100"
     def query():
         response = requests.get(API_URL, headers=headers)
         return response.json()
-    data = query()
 
+    for sub in ['United Kingdom']:#subjects_list:
+        file_name = os.path.join('data', get_task_type(task), task, f'{sub.split()[0]}.json')
+        if ' ' in sub:
+            file_name = os.path.join('data', get_task_type(task), task, f'{sub.split()[0]}_{sub.split()[1]}.json')
+            sub = f'%22'+ sub.split()[0] +'%20'+ sub.split()[1] +'%22'
+        query_text = sub
+        print(query_text)
+        API_URL = f"https://datasets-server.huggingface.co/search?dataset=iohadrubin%2Fwikitext-103-raw-v1&config=default&split=train&query={query_text}&offset=0&length=30"
+        data = query()
 
-
-    for sub in tqdm(subjects_list):
-        n = 0
-        while n<5:
-            data = list(dataset.take(1))[0]['text']
-            if sub in data:
-                source_prompts.append({'subject': sub, 'S': data})
-                n += 1
-
-            dataset.to_csv(f'wikitext_{task}.csv', index=False)
+        with open(file_name, 'w', encoding='utf-8') as f:
+            json.dump(data, f, ensure_ascii=False, indent=4)
     
-    return source_prompts
+    return 
+
+
+def truncate_samples(task, model):
+    task_type = get_task_type(task) 
+    data_path = os.path.join('data', get_task_type(task), task)
+
+    # [TODO]
+    # pour tous les json
+        # subject = title.replace('.json', '')
+        # go through rows
+        # trouve occurence de subject
+        # select 20 tokens avant (donc il faut connaître le modèle)
+        # vérifier que le model "correctly encodes the tuple"
+        # save to a dataframe (subject, S)
+
+    # return dataframe de prompt
 
 
 subjects_list, objects_list = get_subobj_from_json()
-source_prompts = get_S_from_wikitext(subjects_list, task='country_currency')
-print(source_prompts.info)
+get_S_from_wikitext(subjects_list, task='country_currency')
 
